b0ef280c710353bd163a8ddc448fb188c994865a
osmancikjan <j.osmancik@gmail.com>
2016-12-08 00:54:57 +0100

    Add files via upload

--- Chess/src/chess/AI.java
+++ Chess/src/chess/AI.java
@@ -6,42 +6,183 @@
 package chess;
 
 import java.awt.Point;
-
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
- * @author Honza
+ * @author OSM0014
  */
 public class AI {
+
     private Movement movement;
-    private static float Pawn_eval = 1; 
-    private static float Rook_eval = 5; 
-    private static float Bishop_eval = 3;
-    private static float Knight_eval = (float) 3.5;
-    private static float Queen_eval = 10;
-    private static float King_eval=100;
-    
+    private Model model;
+    private Move FinalMove;
+    public Point FinalFrom;
+    public Point FinalTo;
+    Point to = new Point();
+    Point to2 = new Point();
+    ArrayList<Move> moves = new ArrayList<Move>();
+    ArrayList<ModelObject> blacks = new ArrayList<ModelObject>();
+    ArrayList<ModelObject> whites = new ArrayList<ModelObject>();
+    ArrayList<Move> catches = new ArrayList<Move>();
+
     public class Move {
-        public Point position;
-        public Point direction;
-        public int evaluation;
+
+        public Point from;
+        public Point to;
+        public Figures.Type type;
+
         public Move() {
-            position = new Point();
-            direction = new Point();
         }
-        public Move(Point pos, Point dir, int eval){
-            position = new Point(pos);
-            direction = new Point(dir);
-            evaluation = eval;
+
+        public Move(Point from, Point to, Figures.Type type) {
+            this.from = from;
+            this.to = to;
+            this.type = type;
+
+        }
+
+        public int GetEvalOf(Figures.Type type) {
+            int res = 0;
+            switch (type) {
+                case King:
+                    res = 1000;
+                    break;
+                case Queen:
+                    res = 10;
+                    break;
+                case Knight:
+                    res = 4;
+                    break;
+                case Bishop:
+                    res = 3;
+                    break;
+                case Rook:
+                    res = 5;
+                    break;
+                case Pawn:
+                    res = 1;
+                    break;
+            }
+            return res;
         }
     }
     //deklarace objektu nejlepsiho tahu
-    
+
     public AI() {
-        Move possibleMove = new Move();
+        model = new Model();
+        movement = new Movement();
+        //Move possibleMove = new Move();
     }
-    public Movement bestMove() {
+
+    private boolean TryCatch(ArrayList<ModelObject> blacks, ArrayList<ModelObject> whites, ArrayList<ModelObject> UpdatedObjects) {
+        catches.clear();
+        for (ModelObject black : blacks) {
+            //System.out.println("Projizdim cerne");
+            for (ModelObject white : whites) {
+                // System.out.println("Projizdim bile");
+                if (movement.MoveThatFigure(Figures.Color.Black, ((Figures) black).getType(), black.getPosition(), white.getPosition(), UpdatedObjects)) {
+                    catches.add(new Move(black.getPosition(), white.getPosition(), ((Figures) white).getType()));
+                    System.out.println("mel bych pridavat / C");
+                }
+            }
+        }
+        if (catches.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void GenerateMoves() {
+        //vsechny mozne tahy v 1 kroku 
+        moves.clear();
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 2; j <= 7; j++) {
+                to = new Point(i, j);
+                for (Object fig : blacks) {
+                    if (movement.MoveThatFigure(Figures.Color.Black, ((Figures) fig).getType(), ((Figures) fig).getPosition(), to, blacks)) {
+                        Move tmp = new Move(((Figures) fig).getPosition(), to, ((Figures) fig).getType());
+                        moves.add(tmp);
+
+                    }
+                } //vsechny tahu v 2 kroku
+
+            }
+        }
+
+    }
+
+    public void MakeMove(ArrayList<ModelObject> objectsOnBoard) {
+        ArrayList<ModelObject> objectsForMovement = new ArrayList<ModelObject>(objectsOnBoard.size());
         
-        return movement;
+        for (Object obj : objectsOnBoard) {
+            objectsForMovement.add((ModelObject) obj);
+        }
+        //rozdeleni na bile / cerne
+        blacks.clear();
+        whites.clear();
+        for (ModelObject object : objectsForMovement) {
+            if (object instanceof Figures) {
+                if (((Figures) object).getColor() == Figures.Color.Black) {
+                    blacks.add(object);
+                } else if (((Figures) object).getColor() == Figures.Color.White) {
+                    whites.add(object);
+                }
+            }
+        }
+        //here
+
+        //Move finalMove = new Move();
+        if (TryCatch(blacks, whites, objectsForMovement)) {
+            Move bestMove = new Move();
+            //int index = 0;
+            
+            int max = 0;
+            for (Move cat : catches) {
+                if(!catches.isEmpty()) {
+                if (cat.GetEvalOf(cat.type) > max) {
+                    max = cat.GetEvalOf(cat.type);
+                    //index = moves.indexOf(cat);
+                    bestMove.from = cat.from;
+                    bestMove.to = cat.to;
+                    bestMove.type = cat.type;
+                    catches.remove(cat);
+                } else {
+                    catches.remove(cat);
+                }
+                } else break;
+            }
+            this.FinalFrom = new Point(bestMove.from);
+            this.FinalTo = new Point(bestMove.to);
+        } else {
+            GenerateMoves();
+            for (int i = 0; i < moves.size(); i++) {
+                System.out.print("from x" + moves.get(i).from + " to " + moves.get(i).to + moves.get(i).type + "\n");
+            }
+
+            Random r = new Random();
+            int maximum = moves.size()-1;
+            int minimum = 0;
+            int ind = r.nextInt(maximum-minimum)+minimum;
+
+            //   FinalMove = new Move(moves.get(index).from,moves.get(index).to,moves.get(index).type);
+            FinalFrom = new Point(moves.get(ind).from);
+            FinalTo = new Point(moves.get(ind).to);
+        }
+        catches.clear();
+        moves.clear();
+        blacks.clear();
+        whites.clear();
+        objectsForMovement.clear();
+    }
+
+    public Point GetFrom() {
+        return FinalFrom;
+    }
+
+    public Point GetTo() {
+        return FinalTo;
     }
 }
--- Chess/src/chess/Chess.java
+++ Chess/src/chess/Chess.java
@@ -7,12 +7,17 @@ package chess;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Optional;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -23,8 +28,10 @@ import javafx.stage.Stage;
  * @author Dro0076
  */
 public class Chess extends Application {
+    
     private Model model;
     private View view;
+    private AI ai;
     private Controller controller;
     private Movement movement;
     private ArrayList<ModelObject> objects = new ArrayList<>();
@@ -33,14 +40,14 @@ public class Chess extends Application {
     boolean sideToMove = true;
     boolean chooseAndPlay = true;
     boolean rightToMove = false;
-    boolean rightToMovePawn = false; 
-   
-
+    boolean rightToMovePawn = false;
+    boolean AImove = true;
+    
     public Chess() {
         model = new Model();
         movement = new Movement();
+        ai = new AI();
     }
-    
     
     @Override
     public void start(Stage primaryStage) {
@@ -48,7 +55,7 @@ public class Chess extends Application {
         Button btnStart = new Button();
         btnStart.setText("Start game");
         btnStart.setOnAction(new EventHandler<ActionEvent>() {
-
+            
             @Override
             public void handle(ActionEvent event) {
                 if (controller.isRunning()) {
@@ -68,7 +75,7 @@ public class Chess extends Application {
         AnchorPane.setLeftAnchor(btnStart, 0.0);
         AnchorPane.setRightAnchor(btnStart, 0.0);
 
-       /*Vyber barvy*/ 
+        /*Vyber barvy*/
         Alert alert = new Alert(AlertType.NONE);
         alert.setTitle("Vyber hrace");
         alert.setHeaderText("Chcete hrat za bile, cerne nebo po siti?");
@@ -76,22 +83,18 @@ public class Chess extends Application {
         
         ButtonType buttonTypeWhite = new ButtonType("Bile");
         ButtonType buttonTypeBlack = new ButtonType("Cerne");
-        ButtonType buttonTypeMP = new ButtonType("Multak");
         ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
         
-        alert.getButtonTypes().setAll(buttonTypeWhite, buttonTypeBlack, buttonTypeMP, buttonTypeCancel);
+        alert.getButtonTypes().setAll(buttonTypeWhite, buttonTypeBlack, buttonTypeCancel);
         
         Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == buttonTypeWhite){
+        if (result.get() == buttonTypeWhite) {
             // ... user chose "One"
         } else if (result.get() == buttonTypeBlack) {
             // ... user chose "Two"
-        } else if (result.get() == buttonTypeMP) {
-            // ... user chose "Three"
         } else {
             // ... user chose CANCEL or closed the dialog
-        }        
-
+        }
         
         Pane root = new Pane();
         Canvas canvas = new Canvas(View.width, View.height);
@@ -100,149 +103,154 @@ public class Chess extends Application {
         canvas.scaleYProperty().bind(root.heightProperty().multiply(1.0 / View.height));
         canvas.translateXProperty().bind(root.widthProperty().subtract(View.width).divide(2));
         canvas.translateYProperty().bind(root.heightProperty().subtract(View.height).divide(2));
-
+        
         view = new View(canvas.getGraphicsContext2D(), model);
         controller = new Controller(view, model);
         canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                     
-                     if(chooseAndPlay==true)
-                     {
-                       int xosa = (int)event.getSceneX();
-                         int yosa = (int)event.getSceneY();
-                         xosa = xosa/90;
-                         yosa = yosa/90;
-                         
-                         pozice = new Point(xosa,yosa);
-                            for (ModelObject object : model.getObjects()) {
-                                if( object.position.equals(pozice))
-                                    { 
-                                     if(object instanceof Figures){
-                                   
-                                     if (((Figures) object).getType() == Figures.Type.Pawn )
-                                     {
-                                       System.out.println("Vybral jsi pincla na pozici "+pozice);   
-                                     }
-                                     if (((Figures) object).getType() == Figures.Type.Rook )
-                                     {
-                                       System.out.println("Vybral jsi vez na pozici "+pozice);            
-                                     }
-                                      if (((Figures) object).getType() == Figures.Type.Bishop )
-                                     {
-                                       System.out.println("Vybral jsi strelce na pozici "+pozice);            
-                                     }
-                                     if (((Figures) object).getType() == Figures.Type.Knight )
-                                     {
-                                       System.out.println("Vybral jsi kone na pozici "+pozice);            
-                                     }
-                                     if (((Figures) object).getType() == Figures.Type.King )
-                                     {
-                                       System.out.println("Vybral jsi krale na pozici "+pozice);            
-                                     }
-                                     if (((Figures) object).getType() == Figures.Type.Queen )
-                                     {
-                                       System.out.println("Vybral jsi kralovnu na pozici "+pozice);            
-                                     }
-                                      chooseAndPlay= false;
-                                    }
-                                 } 
-                            }    
-                     }else{
-                     
-                     rightToMove = false;
-                     rightToMovePawn = false; 
-                     int xosa = (int)event.getSceneX();
-                     int yosa = (int)event.getSceneY();
-                     xosa = xosa/90;
-                     yosa = yosa/90;  
-                     //Figures.Color coloro = null;
-                     
-                     direction = new Point(xosa,yosa);
-                     for (ModelObject object : model.getObjects()) {
-                      
-                      if( object.position.equals(pozice))
-                        {    
-                          if(object instanceof Figures)  
-                          {
-                            if(movement.MoveThatFigure(((Figures) object).getColor(),((Figures) object).getType(),pozice,direction,model.getObjects())){
-                                 //coloro = ((Figures) object).getColor();
-                                    if(((Figures) object).getType() == Figures.Type.Pawn)
-                                     { 
-                                        rightToMovePawn=true;                                     
-                                     }
-                                    else if (((Figures) object).getType() !=Figures.Type.Pawn){
-                                        rightToMove=true;  
-                                   }
-                                }
-                          }
-                        }
-                       }
-                   
-             if(chooseAndPlay==false){ 
-                
-                    if(model.isFree(direction)) {
-                        if(rightToMovePawn==true){  
-                        model.getObjectAt1(pozice,direction);
-                       // model.remove(pozice);
+                if (movement.isKingTaken(model.getObjects())) {
+                    System.out.println("Konec hry");
+                    Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Game Over");
+                    alert.setHeaderText("The king has been taken");
+                    alert.setContentText("Game Over");
+                    alert.showAndWait();
+                    System.exit(0);
+                }
+                if (AImove == false) {
+                    ai.MakeMove(model.getObjects());
+                    if (model.isFree(ai.GetTo())) {
+                        model.getObjectAt1(ai.GetFrom(), ai.GetTo());
+                    } else {
+                        model.remove(ai.GetTo());
+                        model.getObjectAt1(ai.GetFrom(), ai.GetTo());
                     }
-                    }else{
-                        if(rightToMovePawn==true && movement.PawnTaking()==true){ 
-                             // if(coloro==model.getColorOfDirection(direction)){
+                    AImove = true;
+                    view.update();
+                } else {
+                    if (chooseAndPlay == true) {
+                        int xosa = (int) event.getSceneX();
+                        int yosa = (int) event.getSceneY();
+                        xosa = xosa / 90;
+                        yosa = yosa / 90;
+                        
+                        pozice = new Point(xosa, yosa);
+                        for (ModelObject object : model.getObjects()) {
+                            if (object.position.equals(pozice)) {
+                                if (object instanceof Figures) {
+                                    
+                                    if (((Figures) object).getType() == Figures.Type.Pawn) {
+                                        System.out.println("Vybral jsi pincla na pozici " + pozice);
+                                    }
+                                    if (((Figures) object).getType() == Figures.Type.Rook) {
+                                        System.out.println("Vybral jsi vez na pozici " + pozice);
+                                    }
+                                    if (((Figures) object).getType() == Figures.Type.Bishop) {
+                                        System.out.println("Vybral jsi strelce na pozici " + pozice);
+                                    }
+                                    if (((Figures) object).getType() == Figures.Type.Knight) {
+                                        System.out.println("Vybral jsi kone na pozici " + pozice);
+                                    }
+                                    if (((Figures) object).getType() == Figures.Type.King) {
+                                        System.out.println("Vybral jsi krale na pozici " + pozice);
+                                    }
+                                    if (((Figures) object).getType() == Figures.Type.Queen) {
+                                        System.out.println("Vybral jsi kralovnu na pozici " + pozice);
+                                    }
+                                    chooseAndPlay = false;
+                                }
+                            }
+                        }
+                    } else {
+                        AImove = false;
+                        rightToMove = false;
+                        rightToMovePawn = false;
+                        int xosa = (int) event.getSceneX();
+                        int yosa = (int) event.getSceneY();
+                        xosa = xosa / 90;
+                        yosa = yosa / 90;
+                        //Figures.Color coloro = null;
+
+                        direction = new Point(xosa, yosa);
+                        for (ModelObject object : model.getObjects()) {
+                            
+                            if (object.position.equals(pozice)) {
+                                if (object instanceof Figures) {
+                                    if (movement.MoveThatFigure(((Figures) object).getColor(), ((Figures) object).getType(), pozice, direction, model.getObjects())) {
+                                        //coloro = ((Figures) object).getColor();
+                                        if (((Figures) object).getType() == Figures.Type.Pawn) {
+                                            rightToMovePawn = true;
+                                        } else if (((Figures) object).getType() != Figures.Type.Pawn) {
+                                            rightToMove = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        
+                        if (chooseAndPlay == false) {
+                            
+                            if (model.isFree(direction)) {
+                                if (rightToMovePawn == true) {
+                                    model.getObjectAt1(pozice, direction);
+                                    // model.remove(pozice);
+                                }
+                            } else if (rightToMovePawn == true && movement.PawnTaking() == true) {
+                                // if(coloro==model.getColorOfDirection(direction)){
                                 model.remove(direction);
-                                model.getObjectAt1(pozice,direction);
+                                model.getObjectAt1(pozice, direction);
                                 //model.remove(pozice);
-                            /*  }else{
+                                /*  }else{
                                 System.out.println("Beres svojeho vole");
                                 coloro=null;
                               }*/
-                    }
-                    }
-                    if(rightToMove==true){
-                            if(model.isFree(direction)){
-                                model.getObjectAt1(pozice,direction);
-                                model.remove(pozice);
                             }
-                           else{
-                             // if(coloro==model.getColorOfDirection(direction)){
-                                model.remove(direction);
-                                model.getObjectAt1(pozice,direction);
-                             /* }else{
+                            if (rightToMove == true) {
+                                if (model.isFree(direction)) {
+                                    model.getObjectAt1(pozice, direction);
+                                    model.remove(pozice);
+                                } else {
+                                    // if(coloro==model.getColorOfDirection(direction)){
+                                    model.remove(direction);
+                                    model.getObjectAt1(pozice, direction);
+                                    /* }else{
                                 System.out.println("Beres svojeho vole");
                                 coloro=null;
                               } */
-                       }
-                  }
-                  else if(rightToMove==false && rightToMovePawn==false){                    
-                        System.out.println("Nepovoleny tah, nebo je na tahu protivnik");
-                    }
-                    
-                   /* for (ModelObject object: model.getObjects()) {
+                                }
+                            } else if (rightToMove == false && rightToMovePawn == false) {
+                                System.out.println("Nepovoleny tah, nebo je na tahu protivnik");
+                            }
+
+                            /* for (ModelObject object: model.getObjects()) {
                      System.out.println("x"+object.getX() + "y" + object.getY());
                      }*/
-               }
-               chooseAndPlay = true;    
-           }
-           view.update();               
-        }
-    });
-        
+                        }
+                        chooseAndPlay = true;
+                    }
+                    view.update();
+                }
+            }
+            
+        });
         
         basePane.getChildren().add(root);
         AnchorPane.setBottomAnchor(root, 0.0);
         AnchorPane.setLeftAnchor(root, 0.0);
         AnchorPane.setRightAnchor(root, 0.0);
         AnchorPane.setTopAnchor(root, 30.0);
-
-        Scene scene = new Scene(basePane, 720, 720); 
+        
+        Scene scene = new Scene(basePane, 720, 720);
+        
         primaryStage.setResizable(false);
         primaryStage.setTitle("Chess");
         primaryStage.setScene(scene);
+        
         primaryStage.show();
         
     }
-    
-    
+
     /**
      * @param args the command line arguments
      */
@@ -251,3 +259,5 @@ public class Chess extends Application {
     }
     
 }
+/*model.getObjectAt1(moves.get(index).from, moves.get(index).to);
+                                model.remove(moves.get(index).from);*/
